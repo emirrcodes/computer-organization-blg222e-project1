e75e4e3ef0ba19a7703b7077c062f7b254cdee21
emirrcodes <emirarslanonline@gmail.com>
2025-04-12 20:19:20 +0300

    Update RegisterFile.v

--- RegisterFile.v
+++ RegisterFile.v
@@ -1,65 +1,60 @@
+`timescale 1ns / 1ps
+
+
+// 2/4 geçiyo testlerden
 module RegisterFile (
-    input clk,
+    input Clock,
     input rst,
     input [2:0] FunSel,
     input [3:0] RegSel,
     input [3:0] ScrSel,
     input [2:0] OutASel,
     input [2:0] OutBSel,
-    input [7:0] I,
-    output reg [31:0] OutA,
-    output reg [31:0] OutB
+    input [31:0] I,
+    output [31:0] OutA,
+    output [31:0] OutB
 );
 
-// 8 tane 32-bit register
-reg [31:0] R [0:7]; // R[0]=R1, R[1]=R2, ..., R[7]=S4
+wire [31:0] OutR1, OutR2, OutR3, OutR4;
+wire [31:0] OutS1, OutS2, OutS3, OutS4;
 
-integer i;
+    // 8 adet Register mod�l�
+    Register32bit R1 (.Clock(Clock), .rst(rst), .E(RegSel[0]), .FunSel(FunSel), .I(I), .Q(OutR1));
+    Register32bit R2 (.Clock(Clock), .rst(rst), .E(RegSel[1]), .FunSel(FunSel), .I(I), .Q(OutR2));
+    Register32bit R3 (.Clock(Clock), .rst(rst), .E(RegSel[2]), .FunSel(FunSel), .I(I), .Q(OutR3));
+    Register32bit R4 (.Clock(Clock), .rst(rst), .E(RegSel[3]), .FunSel(FunSel), .I(I), .Q(OutR4));
+    Register32bit S1 (.Clock(Clock), .rst(rst), .E(ScrSel[0]), .FunSel(FunSel), .I(I), .Q(OutS1));
+    Register32bit S2 (.Clock(Clock), .rst(rst), .E(ScrSel[1]), .FunSel(FunSel), .I(I), .Q(OutS2));
+    Register32bit S3 (.Clock(Clock), .rst(rst), .E(ScrSel[2]), .FunSel(FunSel), .I(I), .Q(OutS3));
+    Register32bit S4 (.Clock(Clock), .rst(rst), .E(ScrSel[3]), .FunSel(FunSel), .I(I), .Q(OutS4));
 
-always @(posedge clk) begin
-    if (rst) begin
-        for (i = 0; i < 8; i = i + 1)
-            R[i] <= 32'b0;
-    end
-    else begin
-        for (i = 0; i < 4; i = i + 1) begin // R1-R4
-            if (RegSel[i]) begin
-                R[i] <= apply_function(R[i], FunSel, I);
-            end
-        end
-        for (i = 0; i < 4; i = i + 1) begin // S1-S4
-            if (ScrSel[i]) begin
-                R[i+4] <= apply_function(R[i+4], FunSel, I);
-            end
-        end
-    end
-end
+    // �?k?? se�imleri
+    reg [31:0] outA_temp, outB_temp;
+    assign OutA = outA_temp;
+    assign OutB = outB_temp;
 
-// Çıkış register seçimi
-always @(*) begin
-    OutA = R[OutASel];
-    OutB = R[OutBSel];
-end
+    always @(*) begin
+        case (OutASel)
+            3'd0: outA_temp = OutR1;
+            3'd1: outA_temp = OutR2;
+            3'd2: outA_temp = OutR3;
+            3'd3: outA_temp = OutR4;
+            3'd4: outA_temp = OutS1;
+            3'd5: outA_temp = OutS2;
+            3'd6: outA_temp = OutS3;
+            3'd7: outA_temp = OutS4;
+        endcase
 
-// Fonksiyon modülü
-function [31:0] apply_function;
-    input [31:0] current;
-    input [2:0] fs;
-    input [7:0] data;
-
-    begin
-        case (fs)
-            3'b000: apply_function = current - 1;
-            3'b001: apply_function = current + 1;
-            3'b010: apply_function = {24'b0, data}; // Load
-            3'b011: apply_function = 32'b0;         // Clear
-            3'b100: apply_function = {24'b0, data}; // Upper Clear
-            3'b101: apply_function = {16'b0, data, 8'b0}; // Clear upper 16 bits, load lower 16
-            3'b110: apply_function = {current[23:0], data}; // Left Shift
-            3'b111: apply_function = {{16{data[7]}}, data, 8'b0}; // Sign Extend
-            default: apply_function = current;
+        case (OutBSel)
+            3'd0: outB_temp = OutR1;
+            3'd1: outB_temp = OutR2;
+            3'd2: outB_temp = OutR3;
+            3'd3: outB_temp = OutR4;
+            3'd4: outB_temp = OutS1;
+            3'd5: outB_temp = OutS2;
+            3'd6: outB_temp = OutS3;
+            3'd7: outB_temp = OutS4;
         endcase
     end
-endfunction
 
 endmodule
