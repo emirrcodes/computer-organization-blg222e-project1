2304a088751e7ae55e636364af53cd0cc24da91e
emirrcodes <emirarslanonline@gmail.com>
2025-04-12 17:01:05 +0300

    Update Register16bit.v

--- Register16bit.v
+++ Register16bit.v
@@ -1,22 +1,19 @@
 module Register16bit(
-    input clk,
-    input rst,
+    input Clock,                 // <- İsim uyuşmalı
     input E,
     input [1:0] FunSel,
     input [15:0] I,
     output reg [15:0] Q
 );
 
-always @(posedge clk) begin
-    if (rst)
-        Q <= 16'b0;
-    else if (E) begin
+always @(posedge Clock) begin
+    if (E) begin
         case (FunSel)
             2'b00: Q <= Q - 1;        // Decrement
             2'b01: Q <= Q + 1;        // Increment
-            2'b10: Q <= I;            // Load input
+            2'b10: Q <= I;            // Load
             2'b11: Q <= 16'b0;        // Clear
-            default: Q <= Q;          // Retain (optional)
+            default: Q <= Q;          // Retain (opsiyonel)
         endcase
     end
 end
